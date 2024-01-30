456058add2f37c87567f65c2ad67dddae4ee3001
esilaeva <emsilaeva@gmail.com>
2024-01-30 20:21:21 +0200

    update counter logic

--- src/main/java/com/ilana/bot/repository/UserProgressRepository.java
+++ src/main/java/com/ilana/bot/repository/UserProgressRepository.java
@@ -1,11 +1,9 @@
 package com.ilana.bot.repository;
 
-import com.ilana.bot.model.User;
 import com.ilana.bot.model.UserProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +16,10 @@ public interface UserProgressRepository extends JpaRepository<UserProgress, Long
 
     UserProgress findByChatIdAndWordIdAndLanguage(@Param("chatId") Long chatId, @Param("wordId") Long wordId, @Param("language") String language);
 
-    @Query(value = "SELECT word_Counter FROM User_Progress WHERE language = :language and word_Id = :wordId", nativeQuery = true)
+    @Query(value = "SELECT word_counter FROM user_progress WHERE language = :language and word_id = :wordId", nativeQuery = true)
     Long findCounterByLanguageAndWordId(@Param("language") String language, @Param("wordId") Long wordId);
 
-    @Query(value = "SELECT word_Id FROM User_Progress WHERE chat_Id = :chatId", nativeQuery = true)
+    @Query(value = "SELECT word_id FROM user_progress WHERE chat_id = :chatId", nativeQuery = true)
     List<Long> findWordIdByChatId(@Param("chatId") Long chatId);
 
     @Query("SELECT up FROM UserProgress up WHERE up.language = :language and up.wordId = :wordId")
@@ -35,5 +33,14 @@ public interface UserProgressRepository extends JpaRepository<UserProgress, Long
     @Query("UPDATE UserProgress up SET up.wordCounter = :wordCounter WHERE up.language = :language and up.wordId = :wordId")
     void updateCountWordByLanguageAndWordId(@Param("wordCounter") Long wordCounter, @Param("language") String language, @Param("wordId") Long wordId);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserProgress up SET up.wordCounter = 0 WHERE up.language = :language and up.chatId = :chatId")
+    void updateWordCounterEnByLanguageAndChatId(@Param("language") String language, @Param("chatId") Long chatId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserProgress up SET up.wordCounter = 0 WHERE up.language = :language and up.chatId = :chatId")
+    void updateWordCounterRuByLanguageAndChatId(@Param("language") String language, @Param("chatId") Long chatId);
 
 }
